8262ae8afd297484482cd97045fa3134f0c2b049
Henry Haugsten Hansen <h.h.hansen@fys.uio.no>
2020-05-26 11:08:33 +0200

    Update quaternion_operations.h

--- include/quaternion_operations.h
+++ include/quaternion_operations.h
@@ -3,18 +3,20 @@
 #pragma once
 
  
-#include <Eigen/dense>
-
-using namespace Eigen; 
+#if defined __GNUC__ || defined __APPLE__
+#include <Eigen/Dense>
+#else
+#include <eigen3/Eigen/Dense>
+#endif
 
 
 /////////////////////////////////////////////////////////////////////////////
 
 
-void make_rotation_matrix_from_quaternion(const Vector4d & quaternion, Matrix3d & rotation_matrix); 
+void make_rotation_matrix_from_quaternion(const Eigen::Vector4d & quaternion, Eigen::Matrix3d & rotation_matrix); 
 
 
-void normalize_quaternion(const Vector4d & quaternion, Vector4d & normalized_quaternion); 
+void normalize_quaternion(const Eigen::Vector4d & quaternion, Eigen::Vector4d & normalized_quaternion); 
 
 
 /////////////////////////////////////////////////////////////////////////////
