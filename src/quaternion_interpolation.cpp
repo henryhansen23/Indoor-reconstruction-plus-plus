a5bf6c1504b4a6f8f20f3d237143632415419d50
Henry Haugsten Hansen <h.h.hansen@fys.uio.no>
2020-05-26 15:31:08 +0200

    Update quaternion_interpolation.cpp

--- src/quaternion_interpolation.cpp
+++ src/quaternion_interpolation.cpp
@@ -17,140 +17,140 @@ std::vector <std::pair <Eigen::Vector4d, double> > interpolate_quaternions(const
 
 
   
-                              std::vector <std::pair <Eigen::Vector4d, double> > interpolated_quaternions; 
+                                                   std::vector <std::pair <Eigen::Vector4d, double> > interpolated_quaternions; 
   
   
-                              std::vector <Eigen::Vector4d> equal_quaternions; 
+                                                   std::vector <Eigen::Vector4d> equal_quaternions; 
   
-                              std::vector <double> timestamp; 
+                                                   std::vector <double> timestamp; 
   
-                              Eigen::Vector4d previous_quaternion {1000, 1000, 1000, 1000};  // initialization of variable
+                                                   Eigen::Vector4d previous_quaternion {1000, 1000, 1000, 1000};  // initialization of variable
 
-                              Eigen::Vector4d a, b, q, diff_quat; 
+                                                   Eigen::Vector4d a, b, q, diff_quat; 
   
-                              double diff_time, previous_time = 0;
+                                                   double diff_time, previous_time = 0;
 
 
-                              for (std::size_t i = 0; i < quaternions.size(); ++i) {
+                                                   for (std::size_t i = 0; i < quaternions.size(); ++i) {
 
 
-                                  if (previous_quaternion.isApprox(quaternions[i].first)) {
+                                                       if (previous_quaternion.isApprox(quaternions[i].first)) {
                                     
 
-                                     if (equal_quaternions.empty()) {
+                                                          if (equal_quaternions.empty()) {
                                          
 
-                                        equal_quaternions.push_back(previous_quaternion);
+                                                             equal_quaternions.push_back(previous_quaternion);
 
-                                        interpolated_quaternions.pop_back();
+                                                             interpolated_quaternions.pop_back();
 
-                                        timestamp.push_back(previous_time);
-
-
-                                     }
+                                                             timestamp.push_back(previous_time);
 
 
-                                     equal_quaternions.push_back(quaternions[i].first);
-
-                                     timestamp.push_back(quaternions[i].second);
+                                                          }
 
 
-                                  }
+                                                          equal_quaternions.push_back(quaternions[i].first);
+
+                                                          timestamp.push_back(quaternions[i].second);
 
 
-                                  else {
+                                                       }
 
 
-                                       if (!equal_quaternions.empty()) {
+                                                       else {
 
 
-                                          diff_quat = quaternions[i].first - previous_quaternion;
+                                                            if (!equal_quaternions.empty()) {
+
+
+                                                               diff_quat = quaternions[i].first - previous_quaternion;
   
-                                          diff_time = quaternions[i].second - timestamp[0];
+                                                               diff_time = quaternions[i].second - timestamp[0];
 
                                         
-                                          // Slope
+                                                               // Slope
 
-                                          a << diff_quat(0) / diff_time, 
-                                               diff_quat(1) / diff_time, 
-                                               diff_quat(2) / diff_time, 
-                                               diff_quat(3) / diff_time;      
+                                                               a << diff_quat(0) / diff_time, 
+                                                                    diff_quat(1) / diff_time, 
+                                                                    diff_quat(2) / diff_time, 
+                                                                    diff_quat(3) / diff_time;      
                                        
 
-                                          // y-intercept
+                                                               // y-intercept
 
-                                          b << equal_quaternions[0](0) - a(0) * timestamp[0], 
-                                               equal_quaternions[0](1) - a(1) * timestamp[0], 
-                                               equal_quaternions[0](2) - a(2) * timestamp[0], 
-                                               equal_quaternions[0](3) - a(3) * timestamp[0]; 
-
-
-                                          for (int j = 0; j < timestamp.size(); ++j) {
+                                                               b << equal_quaternions[0](0) - a(0) * timestamp[0], 
+                                                                    equal_quaternions[0](1) - a(1) * timestamp[0], 
+                                                                    equal_quaternions[0](2) - a(2) * timestamp[0], 
+                                                                    equal_quaternions[0](3) - a(3) * timestamp[0]; 
 
 
-                                              // y = ax + b
-
-                                              q << a(0) * timestamp[j] + b(0), 
-                                                   a(1) * timestamp[j] + b(1), 
-                                                   a(2) * timestamp[j] + b(2), 
-                                                   a(3) * timestamp[j] + b(3); 
-
-                                              interpolated_quaternions.push_back(std::make_pair(q, timestamp[j]));
+                                                               for (int j = 0; j < timestamp.size(); ++j) {
 
 
-                                          }
+                                                                   // y = ax + b
+
+                                                                   q << a(0) * timestamp[j] + b(0), 
+                                                                        a(1) * timestamp[j] + b(1), 
+                                                                        a(2) * timestamp[j] + b(2), 
+                                                                        a(3) * timestamp[j] + b(3); 
+
+                                                                   interpolated_quaternions.push_back(std::make_pair(q, timestamp[j]));
 
 
-                                          interpolated_quaternions.push_back(std::make_pair(quaternions[i].first, quaternions[i].second));
+                                                               }
 
 
-                                          equal_quaternions.clear(); 
+                                                               interpolated_quaternions.push_back(std::make_pair(quaternions[i].first, quaternions[i].second));
 
-                                          timestamp.clear(); 
 
-                                          previous_quaternion = quaternions[i].first; 
+                                                               equal_quaternions.clear(); 
 
-                                          previous_time = quaternions[i].second;
+                                                               timestamp.clear(); 
+
+                                                               previous_quaternion = quaternions[i].first; 
+
+                                                               previous_time = quaternions[i].second;
 
                           
-                                       }
+                                                            }
 
 
-                                       else {
+                                                            else {
 
 
-                                            interpolated_quaternions.push_back(std::make_pair(quaternions[i].first, quaternions[i].second));
+                                                                 interpolated_quaternions.push_back(std::make_pair(quaternions[i].first, quaternions[i].second));
 
-                                            previous_quaternion = quaternions[i].first;
+                                                                 previous_quaternion = quaternions[i].first;
 
-                                            previous_time = quaternions[i].second;
+                                                                 previous_time = quaternions[i].second;
 
 
-                                       }
+                                                            }
 
   
-                                  }
+                                                       }
 
 
-                              }
+                                                   }
 
            
-                              // Appending the last quaternions if they are all equal
+                                                   // Appending the last quaternions if they are all equal
 
-                              if (!equal_quaternions.empty()) {
+                                                  if (!equal_quaternions.empty()) {
 
 
-                                 for (std::size_t i = 0; i < equal_quaternions.size(); ++i) {
+                                                      for (std::size_t i = 0; i < equal_quaternions.size(); ++i) {
   
-                                     interpolated_quaternions.push_back(std::make_pair(equal_quaternions[i], timestamp[i]));
+                                                          interpolated_quaternions.push_back(std::make_pair(equal_quaternions[i], timestamp[i]));
 
-                                 }
+                                                      }
 
 
-                              }
+                                                   }
                                             
                               
-                              return interpolated_quaternions; 
+                                                   return interpolated_quaternions; 
 
 
 }
