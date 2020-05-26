fc3a5429f2589f263752095f494893c6a7d9e60a
Henry Haugsten Hansen <h.h.hansen@fys.uio.no>
2020-05-26 11:10:13 +0200

    Update load_data.h

--- include/load_data.h
+++ include/load_data.h
@@ -11,15 +11,17 @@
 #include <pcl/point_cloud.h>
 
 
-#include <Eigen/dense>
+#if defined __GNUC__ || defined __APPLE__
+#include <Eigen/Dense>
+#else
+#include <eigen3/Eigen/Dense>
+#endif
  
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
 std::vector <pcl::PointCloud <pcl::PointXYZ> > load_fragments(const std::string fragments_path); 
-
-std::vector <pcl::PointCloud <pcl::PointXYZ> > load_scans(const std::string path);
 
 std::vector <std::vector <pcl::PointCloud <pcl::PointXYZ> > > load_datapackets(const std::string path);
 
