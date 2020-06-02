aee09dc7c654d0464a4b4829a3dda2e5e8d6168a
Henry Haugsten Hansen <h.h.hansen@fys.uio.no>
2020-06-02 10:34:59 +0200

    Update transformation.cpp

--- src/transformation.cpp
+++ src/transformation.cpp
@@ -10,21 +10,16 @@
 #endif
 
 
-#include "quaternion_operations.h"
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void make_rotation_matrix_from_quaternion(const Eigen::Vector4d & quaternion, Eigen::Matrix3d & rotation_matrix) {
 
+     double qw = quaternion(0), qx = quaternion(1), qy = quaternion(2), qz = quaternion(3); 
 
-void make_rotation_matrix(const Eigen::Vector4d quaternion, Eigen::Matrix3d & rotation) {
-
-
-     Eigen::Vector4d quaternion_normalized; 
-
-     normalize_quaternion(quaternion, quaternion_normalized); 
-
-     make_rotation_matrix_from_quaternion(quaternion_normalized, rotation); 
-
+     rotation_matrix << 1 - 2 * pow(qy, 2) - 2 * pow(qz, 2), 2 * qx * qy - 2 * qz * qw,                     2 * qx * qz + 2 * qy * qw, 
+                        2 * qx * qy + 2 * qz * qw,           1 - 2 * pow(qx, 2) - 2 * pow(qz, 2),           2 * qy * qz - 2 * qx * qw, 
+                        2 * qx * qz - 2 * qy * qw,           2 * qy * qz + 2 * qx * qw,           1 - 2 * pow(qx, 2) - 2 * pow(qy, 2); 
 
 }
 
@@ -61,7 +56,7 @@ std::vector <Eigen::Matrix4d> make_transformation_matrices(const std::vector <Ei
                               for (std::size_t i = 0; i < quaternions.size(); ++i) {
 
 
-                                  make_rotation_matrix(quaternions[i], tripod_rotation); 
+                                  make_rotation_matrix_from_quaternion(quaternions[i], tripod_rotation); 
 
                                   make_translation_vector(tripod_rotation, tripod_position);  
 
